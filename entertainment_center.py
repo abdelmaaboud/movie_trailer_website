e32099d7ce4d05d864b3ebb43e9fc543e101d7bc
islam abdelmaaboud <islamabdelmaaboud@yahoo.com>
2016-12-25 16:48:18 +0200

    Update entertainment_center.py
    add comments and clean up the indentation

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,10 +1,12 @@
+#import fresh_tomatoes to render html page
 import fresh_tomatoes
+#import class movie
 from media import  Movie
+#create movies objects
 dark_knight= Movie("The Dark knight",
-                " When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice. ",
-               "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
-               "https://www.youtube.com/watch?v=EXeTwQWrcwY")
-
+                   " When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice. ",
+                   "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+                   "https://www.youtube.com/watch?v=EXeTwQWrcwY")
 
 blood_diamond= Movie("Blood Diamond",
                      " A fisherman, a smuggler, and a syndicate of businessmen match wits over the possession of a priceless diamond. ",
@@ -12,9 +14,9 @@ blood_diamond= Movie("Blood Diamond",
                      "https://www.youtube.com/watch?v=XtPX2kXhu7I")
 
 django= Movie("Django Unchained",
-" A fisherman, a smuggler, and a syndicate of businessmen match wits over the possession of a priceless diamond. ",
-"https://images-na.ssl-images-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
-    "https://www.youtube.com/watch?v=eUdM9vrCbow")
+              " A fisherman, a smuggler, and a syndicate of businessmen match wits over the possession of a priceless diamond. ",
+              "https://images-na.ssl-images-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+              "https://www.youtube.com/watch?v=eUdM9vrCbow")
 
 revenant= Movie("the revenant"," A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team. ",
                 "https://images-na.ssl-images-amazon.com/images/M/MV5BY2FmODc2N2QtYmY3MS00YTMwLWI2NGYtZWRmYWVkNjFjZmI0XkEyXkFqcGdeQXVyNTMxMjgxMzA@._V1_.jpg",
@@ -26,15 +28,19 @@ guardians_of_the_galaxy= Movie("Guardians of the Galaxy",
                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAwMjU5OTgxNjZeQTJeQWpwZ15BbWU4MDUxNDYxODEx._V1_SY1000_CR0,0,674,1000_AL_.jpg",
                                "https://www.youtube.com/watch?v=89q_HH-3ghk")
 
-zootopia= Movie("Zootopia"," In a city of anthropomorphic animals, a rookie bunny cop and a cynical con artist fox must work together to uncover a conspiracy. ",
-"https://images-na.ssl-images-amazon.com/images/M/MV5BOTMyMjEyNzIzMV5BMl5BanBnXkFtZTgwNzIyNjU0NzE@._V1_SY1000_SX675_AL_.jpg",
+zootopia= Movie("Zootopia",
+                " In a city of anthropomorphic animals, a rookie bunny cop and a cynical con artist fox must work together to uncover a conspiracy. ",
+                "https://images-na.ssl-images-amazon.com/images/M/MV5BOTMyMjEyNzIzMV5BMl5BanBnXkFtZTgwNzIyNjU0NzE@._V1_SY1000_SX675_AL_.jpg",
                 "https://www.youtube.com/watch?v=c6rP-YP4c5I")
-
+#create new list
 movies_list = []
+#add movies to list
 movies_list.append(dark_knight)
 movies_list.append(django)
 movies_list.append(guardians_of_the_galaxy)
 movies_list.append(revenant)
 movies_list.append(blood_diamond)
 movies_list.append(zootopia)
+#use this list as input to generate html file
+# and open it in browser
 fresh_tomatoes.open_movies_page(movies_list)
