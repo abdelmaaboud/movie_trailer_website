78e685f9c77ca2479245283e496c9e824dab7dc7
islam abdelmaaboud <islamabdelmaaboud@yahoo.com>
2016-12-25 16:49:18 +0200

    Update media.py
    add comments

--- media.py
+++ media.py
@@ -1,8 +1,8 @@
 class Movie():
+    """class Movie contain movie information """
     def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+        """first : movie title - second story line - third : poster url - finally youtube url  """
         self.title=movie_title
         self.storyline=movie_storyline
         self.poster_image_url=poster_image
         self.trailer_youtube_url=trailer_youtube
-
-
